a29ce3d717258d6287964eb6402b617119d9dd1a
GeetaSMath <geetasmath@gmail.com>
2021-10-21 18:39:20 -0600

    Added: welcome to solve Oops problem

---
+++ src/com/bridgelabz/LineComparsion_OopsPrblm.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparsion_OopsPrblm {
+    public static void main(String[] args){
+    System.out.println("Welcome to Oops probelms on LineComparision");
+    }
+}
